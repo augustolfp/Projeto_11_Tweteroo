9312b3535876eb36a0d539d2b57ed2f17dc69c63
Augusto Lopes <augustolfp@gmail.com>
2022-06-20 10:11:15 -0300

    Creates the specified Routes templates

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.post("/sign-up", (req, res) => {
+
+});
+
+app.get("/tweets", (req, res) => {
+
+});
+
+app.post("/tweets", (req, res) => {
+
+});
+
+app.listen(5000);
