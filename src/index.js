796cec0f29f2b58bf07cbcbc67fb9b91b8da35f0
Augusto Lopes <augustolfp@gmail.com>
2022-06-20 10:38:54 -0300

    Configures cors and JSON middleware for POST verbs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 let users = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body;
     users.push(user);
+    console.log(user);
     res.send("OK");
 });
 
